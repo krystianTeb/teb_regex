419d3c45b0dbbbbcdc0e7ee1afbf3992d6b57a8e
Krystian <krystian.lewandowski@teb.edu.pl>
2024-02-17 09:43:47 +0100

    2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,13 +10,20 @@ public class Main {
         var regex = "\\d";
         Pattern pattern = Pattern.compile(regex);
 
-        // Start - Pobieranie wartości od użytkownika
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Proszę o podanie cyfry od 0-9: ");
-        String liczba = scan.next();
-        // Koniec - Pobieranie wartości od użytkownika
-        Matcher matcher = pattern.matcher(liczba);
-        System.out.println(matcher.matches());
+        while(true) {
+            // Start - Pobieranie wartości od użytkownika
+            Scanner scan = new Scanner(System.in);
+            System.out.println("Proszę o podanie cyfry od 0-9: ");
+            String liczba = scan.next();
+            // Koniec - Pobieranie wartości od użytkownika
+            Matcher matcher = pattern.matcher(liczba);
+
+            if(matcher.matches() == false) {
+                System.out.println("Podałeś złą wartość. Wprowadź cyfrę jeszcze raz!");
+            }else{
+                break;
+            }
+        }
 
     }
 }
